3ec55d3e81a2b13eaf42d3d1523c6c3f6111ec95
Nick <lolipop22234@gmail.com>
2023-09-25 21:27:21 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        int price = 26789; // Цена билета
+        int mile = 20; // Количество рублей для одной бонусной мили
+        int bonus = price / mile; // Рассчёт количества бонусных миль
+        System.out.println("Количество бонусных миль: " + bonus);
+    }
 }
